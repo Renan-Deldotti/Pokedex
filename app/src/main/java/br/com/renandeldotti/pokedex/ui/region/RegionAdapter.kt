d7ec44a8b4a4c0270f26cfdea31f92df89331fd5
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-07-06 00:59:06 -0300

    Update

---
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionAdapter.kt
@@ -0,0 +1,40 @@
+package br.com.renandeldotti.pokedex.ui.region
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.renandeldotti.pokedex.R
+import kotlinx.android.synthetic.main.item_region.view.*
+
+class RegionAdapter(private val regionsList: List<String>, private val regionListener: RegionListener): RecyclerView.Adapter<RegionAdapter.RegionHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RegionHolder {
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_region, parent, false)
+        return RegionHolder(view, regionListener)
+    }
+
+    override fun getItemCount(): Int {
+        return regionsList.size
+    }
+
+    override fun onBindViewHolder(holder: RegionHolder, position: Int) {
+        holder.textViewRegion.text = regionsList[position]
+    }
+
+    class RegionHolder(itemView: View, regionListener: RegionListener) :RecyclerView.ViewHolder(itemView){
+        var textViewRegion = itemView.textView_region
+
+        init {
+            itemView.setOnClickListener{
+                if (adapterPosition != RecyclerView.NO_POSITION){
+                    regionListener.selectedRegion(adapterPosition)
+                }
+            }
+        }
+    }
+
+    interface RegionListener{
+        fun selectedRegion(position:Int)
+    }
+}
--- app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionFragment.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import br.com.renandeldotti.pokedex.R
+import kotlinx.android.synthetic.main.fragment_region.view.*
 
 
-class RegionFragment : Fragment() {
+class RegionFragment : Fragment(), RegionAdapter.RegionListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_region, container, false)
+    private lateinit var regionsList: List<String>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        val view:View = inflater.inflate(R.layout.fragment_region, container, false)
+
+        regionsList = ArrayList()
+        (regionsList as ArrayList<String>).add("Kanto")
+        (regionsList as ArrayList<String>).add("Kanto")
+        (regionsList as ArrayList<String>).add("Kanto")
+        (regionsList as ArrayList<String>).add("Kanto")
+        (regionsList as ArrayList<String>).add("Kanto")
+
+        view.recyclerView_regions.layoutManager = GridLayoutManager(context,2)
+        view.recyclerView_regions.setHasFixedSize(true)
+        view.recyclerView_regions.adapter = RegionAdapter(regionsList, this)
+
+        return view
     }
 
     companion object {
+    }
+
+    override fun selectedRegion(position: Int) {
+        Toast.makeText(context, "test+"+regionsList[position], Toast.LENGTH_SHORT).show()
     }
 }
