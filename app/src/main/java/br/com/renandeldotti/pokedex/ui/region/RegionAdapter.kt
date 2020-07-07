75ea5674b095169acf668bd898cc797f157eca86
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-07-06 23:49:20 -0300

    Update RegionAdapter.kt

--- app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionAdapter.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionAdapter.kt
@@ -3,15 +3,19 @@ package br.com.renandeldotti.pokedex.ui.region
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.renandeldotti.pokedex.R
 import kotlinx.android.synthetic.main.item_region.view.*
-import java.util.*
 
-class RegionAdapter(private val regionsList: List<String>, private val regionListener: RegionListener): RecyclerView.Adapter<RegionAdapter.RegionHolder>() {
+class RegionAdapter(
+    private val regionsList: List<String>,
+    private val regionListener: RegionListener
+) : RecyclerView.Adapter<RegionAdapter.RegionHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RegionHolder {
-        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_region, parent, false)
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_region, parent, false)
         return RegionHolder(view, regionListener)
     }
 
@@ -23,19 +27,20 @@ class RegionAdapter(private val regionsList: List<String>, private val regionLis
         holder.textViewRegion.text = regionsList[position]
     }
 
-    class RegionHolder(itemView: View, regionListener: RegionListener) :RecyclerView.ViewHolder(itemView){
-        var textViewRegion = itemView.textView_region
+    class RegionHolder(itemView: View, regionListener: RegionListener) :
+        RecyclerView.ViewHolder(itemView) {
+        var textViewRegion: TextView = itemView.textView_region
 
         init {
-            itemView.setOnClickListener{
-                if (adapterPosition != RecyclerView.NO_POSITION){
+            itemView.setOnClickListener {
+                if (adapterPosition != RecyclerView.NO_POSITION) {
                     regionListener.selectedRegion(adapterPosition)
                 }
             }
         }
     }
 
-    interface RegionListener{
-        fun selectedRegion(position:Int)
+    interface RegionListener {
+        fun selectedRegion(position: Int)
     }
 }
