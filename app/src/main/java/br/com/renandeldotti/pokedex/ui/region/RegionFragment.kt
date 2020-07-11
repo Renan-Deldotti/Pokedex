ba88581e37c17c49f9f9df7e15dbd14d0cdda7f4
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-07-10 22:23:13 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionFragment.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionFragment.kt
@@ -1,7 +1,6 @@
 package br.com.renandeldotti.pokedex.ui.region
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,20 +9,16 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import br.com.renandeldotti.pokedex.R
-import br.com.renandeldotti.pokedex.data.Region
+import br.com.renandeldotti.pokedex.database.Region
 import br.com.renandeldotti.pokedex.databinding.FragmentRegionBinding
-import kotlinx.coroutines.*
-import kotlin.concurrent.thread
-import kotlin.coroutines.coroutineContext
 
 
 class RegionFragment : Fragment(), RegionAdapter.RegionListener {
 
     private lateinit var regionViewModel: RegionViewModel
-    private lateinit var regions:Region
+    private lateinit var regions: List<Region>
     //private var isConnectedToInternet:Boolean = false
     private lateinit var fragmentRegionBinding: FragmentRegionBinding
 
@@ -39,21 +34,20 @@ class RegionFragment : Fragment(), RegionAdapter.RegionListener {
     }
 
     private fun updateRegionsData(){
-        regionViewModel.regionsMutableLiveData.observe(viewLifecycleOwner, Observer {
+        regionViewModel.getRegions().observe(viewLifecycleOwner, Observer {
             it?.let {
-                val regionsList = ArrayList<String>()
-                regionsList.clear()
-                for ( result in it.results){
-                    regionsList.add(result.name)
+                val tempList = ArrayList<String>()
+                for(region in it){
+                    tempList.add(region.regionName)
                 }
-                fragmentRegionBinding.recyclerViewRegions.adapter = RegionAdapter(regionsList, this)
                 regions = it
+                fragmentRegionBinding.recyclerViewRegions.adapter = RegionAdapter(tempList, this)
             }
         })
     }
 
     override fun selectedRegion(position: Int) {
-        Toast.makeText(context, "test+"+regions.results[position].name, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "Name: ${regions[position].regionName}", Toast.LENGTH_SHORT).show()
     }
 
     /*private fun test(){
--- app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionViewModel.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/region/RegionViewModel.kt
@@ -1,26 +1,20 @@
 package br.com.renandeldotti.pokedex.ui.region
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import br.com.renandeldotti.pokedex.api.RetrofitPokeApi
-import br.com.renandeldotti.pokedex.data.Region
 import br.com.renandeldotti.pokedex.database.PokeRepository
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import br.com.renandeldotti.pokedex.database.Region
 
 class RegionViewModel(application: Application) : AndroidViewModel(application) {
     private val pokeRepository:PokeRepository = PokeRepository(application)
-    val regionsMutableLiveData:MutableLiveData<Region> = MutableLiveData()
 
     companion object{
         val TAG:String = RegionViewModel::class.java.simpleName
     }
 
-    fun getAllRegions():LiveData<List<br.com.renandeldotti.pokedex.database.Region>> = pokeRepository.getAllRegions()
+    fun getRegions(): LiveData<List<Region>> = pokeRepository.getAllRegions()
 
     override fun onCleared() {
         super.onCleared()
