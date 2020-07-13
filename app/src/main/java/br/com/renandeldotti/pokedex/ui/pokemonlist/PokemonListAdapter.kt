6c44202aeb10ca83fbeef9dc32836d09a219a9c6
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-07-13 00:33:18 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/pokedex/ui/pokemonlist/PokemonListAdapter.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/pokemonlist/PokemonListAdapter.kt
@@ -1,7 +1,6 @@
 package br.com.renandeldotti.pokedex.ui.pokemonlist
 
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,9 +12,9 @@ import br.com.renandeldotti.pokedex.api.data.PokemonEntries
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_pokemon.view.*
 import java.net.URI
-import java.util.*
 
-class PokemonListAdapter(private val pokemonList:List<PokemonEntries>): RecyclerView.Adapter<PokemonListAdapter.PokemonListViewHolder>() {
+class PokemonListAdapter(private val pokemonList: List<PokemonEntries>) :
+    RecyclerView.Adapter<PokemonListAdapter.PokemonListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonListViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_pokemon, parent, false)
@@ -26,17 +25,20 @@ class PokemonListAdapter(private val pokemonList:List<PokemonEntries>): Recycler
         return pokemonList.size
     }
 
-    @ExperimentalStdlibApi
     override fun onBindViewHolder(holder: PokemonListViewHolder, position: Int) {
         val thisPokemon = pokemonList[position].pokemon_species
         //Log.e("TAG", "onBindViewHolder: $thisPokemon" )
-        if (!TextUtils.isEmpty(thisPokemon.name)){
-            holder.pokemonName.text = thisPokemon.name.capitalize(Locale.getDefault())
-        }else{
+        if (!TextUtils.isEmpty(thisPokemon.name)) {
+            holder.pokemonName.text = thisPokemon.name
+        } else {
             holder.pokemonName.setText(R.string.no_name)
         }
-        val pokeId:String = URI(thisPokemon.url).path.substringBeforeLast('/').substringAfterLast('/')
-        Glide.with(holder.pokemonImage).load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokeId.png").placeholder(R.drawable.ditto_shiny_placeholder).into(holder.pokemonImage)
+        val pokeId: String =
+            URI(thisPokemon.url).path.substringBeforeLast('/').substringAfterLast('/')
+        Glide.with(holder.pokemonImage)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$pokeId.png")
+            .placeholder(R.drawable.ditto_shiny_placeholder)
+            .into(holder.pokemonImage)
     }
 
 
