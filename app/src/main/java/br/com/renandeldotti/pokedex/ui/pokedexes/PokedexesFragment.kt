053961e8b034067acd899fe6b0fc51895540fab6
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-07-22 23:28:34 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/pokedex/ui/pokedexes/PokedexesFragment.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/pokedexes/PokedexesFragment.kt
@@ -1,12 +1,10 @@
 package br.com.renandeldotti.pokedex.ui.pokedexes
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -22,10 +20,6 @@ class PokedexesFragment : Fragment(), PokedexesAdapter.PokedexAdapterListener {
     private lateinit var binding: FragmentPokedexesBinding
     private lateinit var viewModel: PokedexesViewModel
     private val args: PokedexesFragmentArgs by navArgs()
-
-    companion object{
-        private const val TAG = "PokedexesFragment"
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/br/com/renandeldotti/pokedex/ui/pokemondetail/PokemonDetailFragment.kt
+++ app/src/main/java/br/com/renandeldotti/pokedex/ui/pokemondetail/PokemonDetailFragment.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
